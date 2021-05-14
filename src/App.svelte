38acb4e90706f73dda078ebbb1fcc10730c76238
Timothy Foster <tf.auroratide@gmail.com>
2021-05-14 09:12:55 -0400

    Rotating symbol

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,8 @@
 <script>
 	import GuildSymbol from './GuildSymbol.svelte'
-
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-
 	<GuildSymbol />
 </main>
 
@@ -17,18 +12,5 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
--- src/GuildSymbol.svelte
+++ src/GuildSymbol.svelte
@@ -1,5 +1,47 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="-110 -110 220 220">
-    <path d="
+<script>
+    let dragging = false
+    let startPosition = {
+        x: 0, y: 0
+    }
+    let rotation = 0
+
+    const startDrag = (e) => {
+        dragging = true
+        startPosition = {
+            x: e.clientX, y: e.clientY
+        }
+    }
+
+    const endDrag = () => {
+        dragging = false
+        rotation = 0;
+    }
+
+    const drag = (e) => {
+        if (dragging) {
+            const dx = e.clientX - startPosition.x
+            const dy = e.clientY - startPosition.y
+            rotation = Math.sqrt(dx * dx + dy * dy)
+        }
+    }
+</script>
+
+<svg
+    on:mousedown={startDrag}
+    on:mouseup={endDrag}
+    on:mousemove={drag}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="-110 -110 220 220"
+>
+    <path class="bottom" d="
+        M -25 0
+        A 25 25 0 0 1 25 0
+        M 0 -25
+        L 0 -100
+        A 100 100 0 0 1 0 100
+    " />
+
+    <path class="top" class:dragging style="transform: rotate({rotation}deg);" d="
         M -25 0
         A 25 25 0 0 1 25 0
         M 0 -25
@@ -10,9 +52,18 @@
 
 <style>
     svg {
-        width: 100px;
-        height: 100px;
+        width: 100%;
+        height: 100%;
     }
+
+    .top {
+        transition: transform 400ms ease-in-out;
+    }
+
+    .top.dragging {
+        transition: none;
+    }
+
     path {
         stroke: black;
         stroke-width: 10;
