36f172611f7fd4d198a7046f5b9f249f08bcb192
Timothy Foster <tf.auroratide@gmail.com>
2021-05-14 08:47:50 -0400

    Basic guild symbol

---
+++ src/GuildSymbol.svelte
@@ -0,0 +1,21 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="-110 -110 220 220">
+    <path d="
+        M -25 0
+        A 25 25 0 0 1 25 0
+        M 0 -25
+        L 0 -100
+        A 100 100 0 0 1 0 100
+    " />
+</svg>
+
+<style>
+    svg {
+        width: 100px;
+        height: 100px;
+    }
+    path {
+        stroke: black;
+        stroke-width: 10;
+        fill: none;
+    }
+</style>
